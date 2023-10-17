fd8e94a8a791ce12d213caa141546e818ba447d9
qazizohaibali <zebikazi33@gmail.com>
2023-10-18 02:22:10 +0500

    feat: herosection has been integrated

--- web/src/sections/HeroSection/HeroSection.jsx
+++ web/src/sections/HeroSection/HeroSection.jsx
@@ -2,12 +2,19 @@ import React from 'react'
 import clsx from 'clsx'
 import { graphql } from 'gatsby'
 import Image from '../../components/Image/Image'
+import Heading from '../../components/Heading'
+import RichText from '../../components/RichText/RichText'
+
+import './herosection.scss'
+import Button from '../../components/Button'
+import Icon from '../../components/Icon/Icon'
 
 export const HeroSection = ({
   otherClasses,
   mainHeading,
   image,
-  buttons,
+  // firstButton,
+  // secondButton,
   _rawSubText,
 }) => {
   const heroSectionClasses = clsx(otherClasses)
@@ -16,12 +23,36 @@ export const HeroSection = ({
     <section className={heroSectionClasses} data-testid="hero-section">
       <div className="relative">
         <div className="relative flex">
-          <div className="bg-[blue] w-[30%]">
-            <Image imageData={image} otherClasses="absolute top-0 right-0" />
+          <div className="bg-[#2e3034] w-[30%]">
+            <Image
+              imageData={image}
+              otherClasses="!absolute top-0 right-0 !w-[70%] !h-full"
+            />
           </div>
-          <div className='relative lg:py-20 py-10'>
-          My-content
+
+          <div className="relative lg:py-36 py-10 -translate-x-[200px] xl:-translate-x-[300px] max-w-[300px]">
+            <Heading
+              type="h4"
+              otherClasses="text-[90px] max-w-[300px] leading-[100px]"
+            >
+              {mainHeading}
+            </Heading>
+            <RichText
+              richText={_rawSubText}
+              otherClasses="herosection-richtext my-[20px] lg:my-[30px]"
+            />
+
+            {/* <div className="flex gap-4">
+              {firstButton && <Button {...firstButton} otherClasses="" />}
+              {secondButton && <Button {...secondButton} otherClasses="" />}
+            </div> */}
           </div>
+        </div>
+        <div className="absolute top-[40%] bg-[#424242] rounded-r-xl">
+          <Icon icon="fb" iconHeight={24} iconWidth={24}/>
+          <Icon icon="Insta" iconHeight={24} iconWidth={24} />
+          <Icon icon="LinkedIn" iconHeight={24} iconWidth={24} />
+          <Icon icon="Twitter" iconHeight={24} iconWidth={24} />
         </div>
       </div>
     </section>
@@ -38,7 +69,13 @@ export const query = graphql`
     image {
       ...CustomImage
     }
-    
+
     _rawSubText
   }
 `
+// firstButton {
+//   ...Button
+// }
+// secondButton {
+//   ...Button
+// }
