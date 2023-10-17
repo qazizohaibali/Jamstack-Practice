974a0936804e3314f6459cfe0c2580c9df4a9d1e
qazizohaibali <zebikazi33@gmail.com>
2023-10-18 01:36:18 +0500

    fix: field fix in herosection's scheam

--- sanity-project/schemas/documents/heroSection.js
+++ sanity-project/schemas/documents/heroSection.js
@@ -1,52 +1,59 @@
 export default {
-    title: 'heroSection',
-    name: 'heroSection',
-    type: 'document',
-    fields: [
-      {
-        title: 'Identifier',
-        name: 'identifier',
-        type: 'string',
-        initialValue: 'Career Hero Section',
-      },
-      {
-        title: 'Main Heading',
-        name: 'mainHeading',
-        type: 'string',
-        validation: (Rule) => Rule.required(),
-      },
-      {
-        title: 'Sub Text',
-        name: 'subText',
-        type: 'bodyPortableText',
-        validation: (Rule) => Rule.required(),
-      },
-      {
-        title: 'Image',
-        name: 'image',
-        type: 'customImage',
-        validation: (Rule) => Rule.required(),
-      },
-      {
-        title: 'Buttons',
-        name: 'buttons',
-        type: 'array',
-        of:[{
-          type:'button'
-        }]
-      },
-
-   
-    ],
-    preview: {
-      select: {
-        title: 'identifier',
-      },
-      prepare({title = 'No title'}) {
-        return {
-          title,
-        }
-      },
+  title: 'heroSection',
+  name: 'heroSection',
+  type: 'document',
+  fields: [
+    {
+      title: 'Identifier',
+      name: 'identifier',
+      type: 'string',
+      initialValue: 'Career Hero Section',
     },
-  }
-  
+    {
+      title: 'Main Heading',
+      name: 'mainHeading',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      title: 'Sub Text',
+      name: 'subText',
+      type: 'bodyPortableText',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      title: 'Image',
+      name: 'image',
+      type: 'customImage',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      title: 'First Button',
+      name: 'firstButton',
+      of: [
+        {
+          type: 'button',
+        },
+      ],
+    },
+    {
+      title: 'Second Button',
+      name: 'secondButton',
+      of: [
+        {
+          type: 'button',
+        },
+      ],
+    },
+  ],
+  preview: {
+    select: {
+      title: 'identifier',
+    },
+    prepare({title = 'No title'}) {
+      return {
+        title,
+      }
+    },
+  },
+}
