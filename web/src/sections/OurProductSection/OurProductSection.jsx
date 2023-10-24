8751b80628a99aded7923ed449884bc76f5d4ff5
qazizohaibali <zebikazi33@gmail.com>
2023-10-25 00:30:51 +0500

    feat: ourproductsection is completed

--- web/src/sections/OurProductSection/OurProductSection.jsx
+++ web/src/sections/OurProductSection/OurProductSection.jsx
@@ -4,6 +4,8 @@ import Heading from '../../components/Heading'
 import RichText from '../../components/RichText/RichText'
 import Image from '../../components/Image/Image'
 
+import './ourproductsection.scss'
+
 export const OurProductSection = ({
   otherClasses,
   heading,
@@ -17,7 +19,7 @@ export const OurProductSection = ({
       className={ourProductSectionClasses}
       data-testid="our-product-section"
     >
-      <div className="max-w-[1512px] mx-auto lg:px-[70px] px-4 lg:py-20 py-10">
+      <div className="max-w-[1512px] mx-auto lg:px-[70px] xl:px-[150px] px-4 lg:py-20 py-10">
         <div className="text-center max-w-[800px] mx-auto">
           <Heading otherClasses="" type="h4">
             {heading}
@@ -25,11 +27,11 @@ export const OurProductSection = ({
           <RichText richText={_rawSubText} otherClasses="ourproduct-richtext" />
         </div>
         <div className="h-[2px] bg-[#c4c4c4] my-8"></div>
-        <div className="grid grid-cols-1 lg:grid-cols-3 mx-auto">
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-8  mx-auto">
           {cards.map(({ image, heading, _rawSubText }) => {
             return (
-              <div className="relative mx-auto group h-[350px]">
-                <Image imageData={image} otherClasses="absolute h-[300px]" />
+              <div className="relative w-full px-3 lg:px-0 mx-auto group h-[350px]">
+                <Image imageData={image} otherClasses="absolute w-full h-full " />
                 <div className="relative -top-[90px] -left-[15px] bg-[#d8a171] p-2 !hidden group-hover:!block">
                   <Heading otherClasses="text-[20px]" type="h4" >
                     {heading}
