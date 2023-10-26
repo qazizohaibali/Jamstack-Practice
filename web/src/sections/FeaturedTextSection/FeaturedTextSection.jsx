2ba0116fe5ae920d5c5923196f108e1a3a648576
mudassir303 <mudassirsmart303@gmail.com>
2023-10-27 01:42:45 +0500

    feat: added featured text section

--- web/src/sections/FeaturedTextSection/FeaturedTextSection.jsx
+++ web/src/sections/FeaturedTextSection/FeaturedTextSection.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import clsx from 'clsx'
 import { graphql } from 'gatsby'
+import RichText from '../../components/RichText/RichText'
+import './featuredTextSection.scss'
 
-export const FeaturedTextSection = ({ otherClasses }) => {
-  const featuredTextSectionClasses = clsx(otherClasses)
+export const FeaturedTextSection = ({ otherClasses, _rawSubText }) => {
+  const featuredTextSectionClasses = clsx(otherClasses,'bg-[#2F3034]')
 
   return (
     <section
       className={featuredTextSectionClasses}
       data-testid="featured-text-section"
     >
-      New Section!
+     <div className='max-w-[1200px] mx-auto xl:px-[150px] lg:px-[70px] px-4 py-12'>
+      <RichText otherClasses="featured-section-richText" richText={_rawSubText}/>
+     </div>
     </section>
   )
 }
