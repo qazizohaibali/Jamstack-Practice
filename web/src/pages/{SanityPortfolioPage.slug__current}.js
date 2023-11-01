2cea9b38870f003442273bd4cad8c73d4e8ae672
qazizohaibali <zebikazi33@gmail.com>
2023-11-02 01:42:56 +0500

    file created for portfoliopage template in web

---
+++ web/src/pages/{SanityPortfolioPage.slug__current}.js
@@ -0,0 +1,53 @@
+import { graphql } from 'gatsby'
+import React from 'react'
+import EntryRenderer from '../components/EntryRenderer/EntryRenderer'
+import Layout from '../components/Layout/Layout'
+import Seo from '../components/seo'
+
+const IndexPage = (props) => {
+  return (
+    <Layout isCtaEnabled={true}>
+      <EntryRenderer pageBuilderData={props.data.page.pageBuilder} />
+    </Layout>
+  )
+}
+
+export const Head = ({
+  data: {
+    page: { seoTitle, metaDescription },
+  },
+}) => <Seo title={seoTitle} description={metaDescription} />
+
+export default IndexPage
+
+export const query = graphql`
+  query PortfolioPageQuery($id: String!) {
+    sanityPortfolioPage(id: { eq: $id }) {
+      slug {
+        current
+      }
+      seoTitle
+      metaDescription
+      __typename
+      title
+      _rawBody
+      mainPortfolioImage {
+        ...CustomImage
+      }
+      portfolioCardImages {
+        image {
+          ...CustomImage
+        }
+      }
+      portfolioCategory
+      _rawShortDescription
+      _rawDescription
+      _rawBody
+    }
+    site {
+      siteMetadata {
+        siteUrl
+      }
+    }
+  }
+`
