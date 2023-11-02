0fd6d7971feab6fa7edc191c282f0f298c081c8a
qazizohaibali <zebikazi33@gmail.com>
2023-11-02 11:27:15 +0500

    feat: basic layout of portfoliohubsection is developed

--- web/src/sections/PortolioHubSection/PortolioHubSection.jsx
+++ web/src/sections/PortolioHubSection/PortolioHubSection.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import clsx from 'clsx'
 import { graphql, useStaticQuery } from 'gatsby'
+import Image from '../../components/Image/Image'
+import Heading from '../../components/Heading'
 export const PortolioHubSection = ({ otherClasses, title }) => {
   const portolioHubSectionClasses = clsx(otherClasses)
 
@@ -28,14 +30,51 @@ export const PortolioHubSection = ({ otherClasses, title }) => {
     }
   `)
 
-  console.log('title', title)
+  const mycategories = [
+    'Home Interior',
+    'Villas',
+    'Interior Fit-Out',
+    'Home Decoration',
+  ]
+
+  // console.log('title', title)
 
   return (
     <section
       className={portolioHubSectionClasses}
       data-testid="portolio-hub-section"
     >
-      New Section!
+      <div className="max-w-[1512px] mx-auto lg:px-[70px] px-4 my-10 lg:my-20">
+        <div className="flex ">
+          {mycategories.map((nodes) => {
+            return (
+              <div className="px-4 py-3 w-full flex justify-center items-center border-b-[2px] border-[#AFAFAF] hover:border-black">
+                <p>{nodes}</p>
+              </div>
+            )
+          })}
+        </div>
+        <div className="mt-5 flex ">
+          <div className="w-full relative lg:w-[50%]">
+            {/* <Image imageData={} otherClasses="absolute top-0 left-0 w-full h-full" /> */}
+            <div className="h-[300px] bg-slate-400">s</div>
+            <div className="relative -mt-3 -ml-3">
+              <Heading type="h4" otherClasses=""></Heading>
+            </div>
+          </div>
+          <div className="w-full lg:w-[50%] flex flex-col gap-1">
+            <div className="flex gap-1 h-full">
+              <div className="bg-slate-600 w-full h-full">hello</div>
+              <div className="bg-slate-600 w-full h-full">hello</div>
+            </div>
+
+            <div className="flex gap-1 h-full">
+              <div className="bg-slate-600 w-full h-full">hello</div>
+              <div className="bg-slate-600 w-full h-full">hello</div>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   )
 }
