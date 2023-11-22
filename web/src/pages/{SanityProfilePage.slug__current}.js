5d9ecaf5ca85db55cbe4b33c455c4700de182aee
Ajazkhan10 <99741323+Ajazkhan10@users.noreply.github.com>
2023-11-23 01:43:20 +0500

    feat:addedprofile-page

---
+++ web/src/pages/{SanityProfilePage.slug__current}.js
@@ -0,0 +1,91 @@
+import { graphql } from 'gatsby'
+import React from 'react'
+import ProfileSection from '../sections/ProfileSection/ProfileSection'
+import Layout from '../components/Layout/Layout'
+import Seo from '../components/seo'
+
+const ProfilePageTemplate = (props) => {
+  console.log(props, 'props ijaz')
+  const { data } = props
+  const profile = data && data.sanityProfilePage
+
+  return (
+    <Layout>
+      {profile && <ProfileSection {...profile} />}
+    </Layout>
+  )
+}
+
+export const Head = ({
+  data: {
+    sanityProfilePage: { seoTitle, metaDescription },
+  },
+}) => <Seo title={seoTitle} description={metaDescription} />;
+
+export default ProfilePageTemplate;
+
+export const query = graphql`
+  query ProfilePageSection($id: String!) {
+    sanityProfilePage(id: { eq: $id }) {
+      slug {
+        current
+      }
+      seoTitle
+      metaDescription
+      __typename
+      title
+      publishedAt
+      featured
+      image {
+        ...CustomImage
+      }
+      name
+      atPosition
+      location {
+        identifier
+        __typename
+        city
+        country
+      }
+      experience {
+        identifier
+        __typename
+        experienceYears
+      }
+      _rawShortDescription
+      topProject {
+        slug {
+          current
+        }
+        mainImage {
+          ...CustomImage
+        }
+        portfolioCardImages {
+          image {
+            ...CustomImage
+          }
+        }
+        portfolioCategory
+      }
+      recentProject {
+        slug {
+          current
+        }
+        mainImage {
+          ...CustomImage
+        }
+        portfolioCardImages {
+          image {
+            ...CustomImage
+          }
+        }
+        portfolioCategory
+      }
+    }
+    site {
+      siteMetadata {
+        siteUrl
+      }
+    }
+  }
+`;
