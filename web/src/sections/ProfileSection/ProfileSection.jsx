5036027920b6bdab21967fa8e25315798d86b9a4
Ajazkhan10 <99741323+Ajazkhan10@users.noreply.github.com>
2023-11-23 22:35:00 +0500

    fix:blog post page

--- web/src/sections/ProfileSection/ProfileSection.jsx
+++ web/src/sections/ProfileSection/ProfileSection.jsx
@@ -19,8 +19,8 @@ export const ProfileSection = ({
   const profileSectionClasses = clsx(otherClasses, 'w-full bg-[#2f3034]')
   return (
     <section className={profileSectionClasses} data-testid="profile-section">
-      <div className="max-w-[1512px] mx-auto lg:px-[70px] xl:px-[150px] px-4 lg:py-20 py-10 flex justify-between">
-        <div className="w-[25%] flex flex-col gap-6">
+      <div className="max-w-[1512px] mx-auto lg:px-[70px] xl:px-[150px] px-4 lg:py-20 py-10 flex flex-col items-center md:items-start gap-10 md:gap-0 md:flex-row justify-between">
+        <div className="w-full md:w-[30%] lg:w-[25%] flex flex-col gap-6">
           <Image imageData={image} otherClasses="w-full min-h-[350px] mb-4" />
           <Heading type="h2">{name}</Heading>
           <Heading type="h4">{atPosition}</Heading>
@@ -40,10 +40,10 @@ export const ProfileSection = ({
           </p>
           <div className="h-[1px] w-full bg-white opacity-20"></div>
         </div>
-        <div className="w-[65%] flex flex-col gap-20">
+        <div className="w-full md:w-[60%] lg:w-[65%] flex flex-col gap-20">
           <div className="top flex flex-col gap-10">
             <Heading type="h2">Top Projects</Heading>
-            <div className="grid grid-cols-3 gap-10">
+            <div className="grid grid-cols-1 md:grid-cols-2  lg:grid-cols-3 gap-10">
               {topProject?.map(({ slug, mainImage }, index) => {
                 return (
                   <Link
@@ -51,7 +51,7 @@ export const ProfileSection = ({
                     key={index}
                     className="hover:opacity-70"
                   >
-                    <Image imageData={mainImage} />
+                    <Image imageData={mainImage} otherClasses="w-full" />
                   </Link>
                 )
               })}
@@ -59,7 +59,7 @@ export const ProfileSection = ({
           </div>
           <div className="recent flex flex-col gap-10">
             <Heading type="h2">Recent Projects</Heading>
-            <div className="grid grid-cols-3 gap-10">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
               {recentProject?.map(({ slug, mainImage }, index) => {
                 return (
                   <Link
@@ -67,7 +67,7 @@ export const ProfileSection = ({
                     key={index}
                     className="hover:opacity-70"
                   >
-                    <Image imageData={mainImage} />
+                    <Image imageData={mainImage} otherClasses="w-full" />
                   </Link>
                 )
               })}
