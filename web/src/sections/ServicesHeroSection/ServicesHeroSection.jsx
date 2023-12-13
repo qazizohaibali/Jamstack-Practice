1a83fb054e1cf91ba3e5b89903af013b085845d0
Qazi Zohaib Ali <zebikazi33@gmail.com>
2023-12-14 01:30:35 +0500

    feat: servicesherosection styling wip

--- web/src/sections/ServicesHeroSection/ServicesHeroSection.jsx
+++ web/src/sections/ServicesHeroSection/ServicesHeroSection.jsx
@@ -4,6 +4,9 @@ import { graphql } from 'gatsby'
 import RichText from '../../components/RichText/RichText'
 import Image from '../../components/Image/Image'
 import Button from '../../components/Button'
+import Heading from '../../components/Heading'
+
+import "./servicesherosection.scss"
 
 export const ServicesHeroSection = ({
   otherClasses,
@@ -14,28 +17,36 @@ export const ServicesHeroSection = ({
   _rawFirstSubText,
   _rawSecSubText,
 }) => {
-  const servicesHeroSectionClasses = clsx(otherClasses)
+  const servicesHeroSectionClasses = clsx(otherClasses, 'bg-[#222327] py-16')
 
   return (
     <section
       className={servicesHeroSectionClasses}
       data-testid="services-hero-section"
     >
-      <div className="flex lg:gap-16 xl:gap-[100px] lg:px-[70px] px-4">
-        <div className="w-full lg:w-[65%] flex lg:gap-10">
-          <RichText
-            richText={_rawFirstSubText}
-            otherClasses="serviceshero-FirstSubText w-[30%]"
-          />
-          <Image imageData={image1} otherClasses="w-[70%]" />
+      <div className=" relative flex lg:gap-16 xl:gap-[100px] lg:px-[70px] px-4">
+        <div className="w-full relative lg:w-[65%] ">
+          <div className="flex relative w-full h-full lg:gap-10">
+            <RichText
+              richText={_rawFirstSubText}
+              otherClasses="serviceshero-FirstSubText w-[35%] pt-10"
+            />
+            <Image imageData={image1} otherClasses="w-[65%] max-h-[420px]" />
+          </div>
+          <Heading
+            type="h3"
+            otherClasses="absolute bottom-20 left-5 lg:text-[170px] tracking-wide leading-[140px]"
+          >
+            {mainHeading}
+          </Heading>
         </div>
         <div className="w-full lg:w-[35%]">
           <RichText
             richText={_rawSecSubText}
             otherClasses="serviceshero-SecSubText"
           />
-          {/* <Button {...button} /> */}
-          <Image imageData={image2} otherClasses="" />
+          <Button label="Reach Us" variant="primary" otherClasses="mt-6 mb-14" />
+          <Image imageData={image2} otherClasses="max-h-[400px]" />
         </div>
       </div>
     </section>
@@ -54,7 +65,6 @@ export const query = graphql`
     image2 {
       ...CustomImage
     }
-
     _rawFirstSubText
     _rawSecSubText
   }
